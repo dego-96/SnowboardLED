e620853ecc2ee2da8b6aa0820e1ca78a37b098e0
dego_96 <dego.project@gmail.com>
2015-10-15 08:39:59 +0900

    add header comment

--- SnowboardLED.ino
+++ SnowboardLED.ino
@@ -3,7 +3,7 @@
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
@@ -11,7 +11,7 @@
 #define PIN            6
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      16
+#define NUMPIXELS      120
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
@@ -34,7 +34,7 @@ void loop() {
 
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
 
-  for(int i=0;i<NUMPIXELS;i++){
+  for(int i=0;i < N UMPIXELS;i+ +){
 
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
@@ -45,3 +45,4 @@ void loop() {
 
   }
 }
+
