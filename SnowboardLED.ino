5f34c8ab712e37ffd3457b49fd60ba682411839b
dego_96 <dego.desu@gmail.com>
2015-10-16 01:38:09 +0900

    create cycle test pattern

--- SnowboardLED.ino
+++ SnowboardLED.ino
@@ -5,15 +5,22 @@
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int delayval = 200;
+int delayval = 100;
 
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
 }
 
 void loop() {
-  for(int i=0;i < NUMPIXELS;i++){
-    pixels.setPixelColor(i, pixels.Color(0,150,0)); // green color.
+  int len = 5; // 同時点灯するLEDの個数
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 255, 0)); // green color. 
+    if (i - len > -1 || NUMPIXELS - len + i) {
+      pixels.setPixelColor(i - len, pixels.Color(0, 0, 0));
+    }
+    if (len > i) {
+      pixels.setPixelColor(NUMPIXELS - len + i, pixels.Color(0, 0, 0));
+    }
     pixels.show(); // This sends the updated pixel color to the hardware.
     delay(delayval); // Delay for a period of time (in milliseconds).
   }
