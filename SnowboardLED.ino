51d7046d98a837543afa642e20091619b2454bda
dego_96 <dego.desu@gmail.com>
2015-10-17 01:25:48 +0900

    add acceleration sonsor reading

--- SnowboardLED.ino
+++ SnowboardLED.ino
@@ -1,20 +1,37 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 6
+#define PIN_LED 6
+#define PIN_ACC_X 0
+#define PIN_ACC_Y 1
+#define PIN_ACC_Z 2
 #define NUMPIXELS 60
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN_LED, NEO_GRB + NEO_KHZ800);
 
 int delayval = 100;
 
 void setup() {
+  Serial.begin(9600);
   pixels.begin(); // This initializes the NeoPixel library.
 }
 
 void loop() {
   int len = 5; // 同時点灯するLEDの個数
+  long accx = 0;
+  long accy = 0;
+  long accz = 0;
   for (int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 255, 0)); // green color. 
+    accx = analogRead(PIN_ACC_X);
+    accy = analogRead(PIN_ACC_Y);
+    accz = analogRead(PIN_ACC_Z);
+    Serial.print("X:");
+    Serial.print(accx);
+    Serial.print("  Y:");
+    Serial.print(accy);
+    Serial.print("  Z:");
+    Serial.println(accz);
+//    pixels.setPixelColor(i, pixels.Color(0, 255, 0)); // green color. 
+    pixels.setPixelColor(i, pixels.Color(255*accx/1024, 255*accy/1024, 255*accz/1024));
     if (i - len > -1 || NUMPIXELS - len + i) {
       pixels.setPixelColor(i - len, pixels.Color(0, 0, 0));
     }
